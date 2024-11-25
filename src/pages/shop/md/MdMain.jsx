b66f29fa1d587e0b4210ea7ef1c298d6b57ff24f
alsdk1676 <alsdk6761@gmail.com>
2024-11-25 18:15:11 +0900

    md main 수정

--- src/pages/shop/md/MdMain.jsx
+++ src/pages/shop/md/MdMain.jsx
@@ -3,25 +3,26 @@ import S from './styleMain'
 import { Link } from 'react-router-dom'
 
 const mdBest = [
-  { id: 1, name: "BEST상품 1", price: 65000, image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510"},
-  { id: 2, name: "BEST상품 2", price: 65000, image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510"},
-  { id: 3, name: "BEST상품 3", price: 65000, image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510"}
+  { id: 1, name: "BEST상품 1", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510"},
+  { id: 2, name: "BEST상품 2", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510"},
+  { id: 3, name: "BEST상품 3", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510"}
 ]
 
 const mds = [
-  { id: 4, name: "상품 4", price: 65000, image: "/image4" },
-  { id: 5, name: "상품 5", price: 65000, image: "/image5" },
-  { id: 6, name: "상품 6", price: 65000, image: "/image6" },
-  { id: 7, name: "상품 7", price: 65000, image: "/image7" },
-  { id: 8, name: "상품 8", price: 65000, image: "/image8" },
-  { id: 9, name: "상품 9", price: 65000, image: "/image9" },
-  { id: 10, name: "상품 10", price: 65000, image: "/image10" },
-  { id: 11, name: "상품 11", price: 65000, image: "/image11" },
-  { id: 12, name: "상품 12", price: 65000, image: "/image12" },
-  { id: 13, name: "상품 13", price: 65000, image: "/image13" },
-  { id: 14, name: "상품 14", price: 65000, image: "/image14" },
-  { id: 15, name: "상품 15", price: 65000, image: "/image15" },
+  { id: 4, name: "상품 4", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 5, name: "상품 5", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 6, name: "상품 6", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 7, name: "상품 7", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 8, name: "상품 8", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 9, name: "상품 9", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 10, name: "상품 10", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 11, name: "상품 11", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 12, name: "상품 12", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 13, name: "상품 13", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 14, name: "상품 14", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
+  { id: 15, name: "상품 15", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" },
 ];
+
 const MdMain = () => {
   
   return (
@@ -43,11 +44,36 @@ const MdMain = () => {
               {/* </Link> */}
             </S.BestImage>
             <S.BestTitle>{best.name}</S.BestTitle>
-            <S.BestPrice>{best.price}</S.BestPrice>
+            <S.BestPrice>{best.price}원</S.BestPrice>
           </S.Best>
         ))}
         </S.BestList>
-        </S.BestWrapper>
+      </S.BestWrapper>
+
+      <S.CategoryButton>
+        <div>
+          <button>showU</button>
+          <button>공연</button>
+          <button>뮤지컬</button>
+          <button>영화</button>
+          <button>영화</button>
+          <button>연극</button>
+          <button>밴드</button>
+        </div>
+      </S.CategoryButton>
+
+      <S.MdWrapper>
+        <S.MdList>{mds.map((md) => (
+          <S.Md2 key={md.id}>
+            <S.MdImage>
+              <img src={md.image} />
+            </S.MdImage>
+            <S.MdTitle>{md.name}</S.MdTitle>
+            <S.MdPrice>{md.price}원</S.MdPrice>
+          </S.Md2>
+        ))}
+          </S.MdList>
+      </S.MdWrapper>
     </S.MainWrapper>
   );
 };
--- src/pages/shop/md/styleMain.js
+++ src/pages/shop/md/styleMain.js
@@ -1,26 +1,27 @@
 import styled from "styled-components";
-import { flexCenterColumn } from "../../../global/common";
 
 const S = {};
 
+
   S.MainWrapper = styled.div`
-    width: 100%;
-    height: 100dvh;
     background-color: #000;
   `
 
   S.Md = styled.h1`
-    color: "#ffd400";
+    color: #ffd400;
     font-weight: bold;
+    font-size: 30px;
     text-align: center;
   `
 
-  S.Best = styled.h1`
+  S.Best = styled.h2`
     color : #fff;
     font-weight: bold;
+    font-size: 30px;
   `
 
   S.BestWrapper = styled.div`
+    margin-top : 50px;
   /* align-items: center; */
     /* display: flex;
     flex-direction: row; */
@@ -29,15 +30,24 @@ const S = {};
   S.BestImage = styled.image`
 
   & img {
-    width: 100px;
-    height: 100px;
+    width: 300px;
+    height: 300px;
   }
   `
 
   S.BestList = styled.div`
-    display: flex;
+    /* display: flex;
     flex-direction: row;
-
+    justify-content: space-between ;
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto; */
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
   `
 
   S.Best = styled.div`
@@ -47,6 +57,67 @@ const S = {};
   `
 
   S.BestPrice = styled.p`
-  
+    font-weight: bold;
+
+  `
+
+  S.MdWrapper = styled.div`
+  margin-top: 30px;
+  `
+
+  S.MdList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 20px;
+    width: 100%;
+    max-width: 1000px;
+    margin: 0 auto;
+  `
+
+  S.Md2 = styled.div`
+  `
+
+  S.MdImage = styled.image`
+
+    & img {
+    width: 300px;
+    height: 300px;
+  }
+  `
+
+  S.MdTitle = styled.p`
+
+  `
+
+  S.MdPrice = styled.p`
+    font-weight: bold;
+  `
+
+  S.CategoryButton = styled.div` // 상단 버튼들
+    margin-left: 320px;
+    margin-top: 80px;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 20px;
+
+      div { // 버튼 간격
+          display: flex;
+          gap: 8px; 
+        }
+
+      button { // 버튼 스타일
+          border-radius: 30px;
+          padding: 10px 15px;
+          background-color: black;
+          color: ${({ theme }) => theme.PALLETE.white};
+          border: 1px solid #ffd400;
+          cursor: pointer;
+          
+          &:hover { // 버튼 호버
+              background-color: #ffd400;
+              color: ${({ theme }) => theme.PALLETE.black};
+            }
+        }
   `
 export default S;
+
