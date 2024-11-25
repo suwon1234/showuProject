2644e48e3df8d0fb5119dc61665d1fc8e88fdcfb
minkyong00 <minkyong131@gmail.com>
2024-11-25 18:34:31 +0900

    layout 수정

--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
+import S from '../join/style';
 
 const Layout = () => {
   return (
