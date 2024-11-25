339b323e02afae3808206f161eac6b4e373344e7
jiihyun12 <j22h.h0h@gmail.com>
2024-11-25 15:09:36 +0900

    news main add

---
+++ src/pages/community/news/Reports.js
@@ -0,0 +1,13 @@
+// 제보내역 페이지
+
+import React from 'react';
+
+const Reports = () => {
+  return (
+    <backgroundBlack> 
+      <h1>제보내역 페이지</h1>
+    </backgroundBlack>
+  );
+};
+
+export default Reports;
