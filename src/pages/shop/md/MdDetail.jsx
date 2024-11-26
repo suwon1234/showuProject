6fb9439552270b8f73b755506eb27197d18a5164
alsdk1676 <alsdk6761@gmail.com>
2024-11-26 18:27:23 +0900

    MD 상세페이지 수정

--- src/pages/shop/md/MdDetail.jsx
+++ src/pages/shop/md/MdDetail.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+// MdDetail.js
+import React from 'react'; 
 import S from './styleDetail';
+import Dropdown from './Dropdown' 
 
-const md = 
-  { id: 1, name: "BEST상품 1", price: "65,000", image: "https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510" }
-  const MdDetail = () => {
+
+const MdDetail = () => {
+  const options = ['옵션 1', '옵션 2', '옵션 3']; 
+
   return (
-    <S.Wrapper>
+    <S.DetailContainer>
+      <S.ImageWrapper>
+        <img src="https://shop-phinf.pstatic.net/20240911_83/17260383426329TtFF_JPEG/6410700596837064_1032211939.jpg?type=m510"/>
+      </S.ImageWrapper>
 
-      <button>카트 추가</button>
-      <button>바로 구매</button>
-      <button>문의하기</button>
+    <S.DetailWrapper>
+      <S.MdInfo>
+        <p>뮤지컬</p>
+        <p>2024 베르사유의 장미 프로그램북 스페셜 에디션</p>
+        <p>65,000원</p>
+      </S.MdInfo>
+    
+      <S.Dropdown>
+        <p>옵션 선택</p>
+        <Dropdown options={options} /> 
+      </S.Dropdown>
       
-    </S.Wrapper>
+      <S.ButtonWrapper2>
+        <S.ButtonWrapper>
+          <S.Button1><p>카트 추가</p></S.Button1>
+          <S.Button2><p>바로 구매</p></S.Button2>
+        </S.ButtonWrapper>
+        <S.Button3><p>문의하기</p></S.Button3>
+      </S.ButtonWrapper2>
+    </S.DetailWrapper>
+    </S.DetailContainer>
   );
 };
 
