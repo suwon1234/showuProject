ed4bf9349fa1efd5094f05654aa00be7dc4bbf7a
minkyong00 <minkyong131@gmail.com>
2024-11-28 18:41:57 +0900

    myClass 작업중

--- src/pages/mypage/myClass/MyClass.jsx
+++ src/pages/mypage/myClass/MyClass.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import Lesson from './Lesson';
 
 const MyClass = () => {
   return (
     <div className='container'>
       <div className='wapper'>
-        <p>MY CLASS</p>
-        <div>
-          <ul>
-            <li>수강중인 레슨</li>
-            <li>개설한 레슨</li>
-            <li>상담 신청 내역</li>
-          </ul>
+        <div className='title'>
+          <p>MY CLASS</p>
+          <div className='subTitle'>
+            <ul>
+              <li>수강중인 레슨</li>
+              <li>개설한 레슨</li>
+              <li>상담 신청 내역</li>
+            </ul>
+          </div>
         </div>
       </div>
       <Lesson />
