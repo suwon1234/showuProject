bfc5080b2c806b3dc80954424f5586a6f1460f44
minkyong00 <minkyong131@gmail.com>
2024-12-02 18:42:36 +0900

    ticket 예매 취소 페이지 추가

--- src/pages/mypage/myRes/cancele/CanceleStyle.js
+++ src/pages/mypage/myRes/cancele/CanceleStyle.js
@@ -6,28 +6,33 @@ const S = {};
     margin-top: 40px;
     width: 800px;
     height: auto;
-    `
+  `
+
   S.Thead = styled.thead`
     font-size: 17px;
     background-color: #ffd400;
     font-weight: 700;
   `
+
   S.TrTitle = styled.tr`
     & th {
       color: #333;
       padding: 10px;
     }
   `
+
   S.Tbody = styled.tbody`
     & td {
       padding: 20px 10px;
     }
   `
-  S.Tr = styled.tr`
 
+  S.Tr = styled.tr`
+    
     & td:nth-of-type(4),
     td:last-of-type {
       color: #ffd400;
+      text-align: center;
     }
   `
 
