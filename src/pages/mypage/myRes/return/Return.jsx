a8334316c15be20de01c0ec1903f746da9dbc22b
minkyong00 <minkyong131@gmail.com>
2024-12-03 00:22:47 +0900

    myRes 반납내역 페이지 작업중

--- src/pages/mypage/myRes/return/Return.jsx
+++ src/pages/mypage/myRes/return/Return.jsx
@@ -1,9 +1,76 @@
 import React from 'react';
+import S from './RetrunStyle';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Return = () => {
   return (
     <div>
-      반납내역
+      <S.Container>
+      <S.Warpper>
+        <S.Image>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/props.png"} alt="소품대여" />
+        </S.Image>
+        <S.Content>
+          <S.Title>
+            <p>레트로 캐리어 가죽 여행가방 촬영소품</p>
+            <div className='heartBox'>
+              <FontAwesomeIcon icon={faHeart} className='heart'/>
+              <p>66</p>
+            </div>
+          </S.Title>
+          <p>25,000원</p>
+          <p>4일 남음</p>
+        </S.Content>
+      </S.Warpper>
+      <S.Warpper>
+        <S.Image>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/props.png"} alt="소품대여" />
+        </S.Image>
+        <S.Content>
+          <S.Title>
+            <p>레트로 캐리어 가죽 여행가방 촬영소품</p>
+            <div className='heartBox'>
+              <FontAwesomeIcon icon={faHeart} className='heart'/>
+              <p>66</p>
+            </div>
+          </S.Title>
+          <p>25,000원</p>
+          <p>6일 남음</p>
+        </S.Content>
+      </S.Warpper>
+      <S.Warpper>
+        <S.Image>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/props.png"} alt="소품대여" />
+        </S.Image>
+        <S.Content>
+          <S.Title>
+            <p>레트로 캐리어 가죽 여행가방 촬영소품</p>
+            <div className='heartBox'>
+              <FontAwesomeIcon icon={faHeart} className='heart'/>
+              <p>66</p>
+            </div>
+          </S.Title>
+          <p>25,000원</p>
+        </S.Content>
+      </S.Warpper>
+      <S.Warpper>
+        <S.Image>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/props.png"} alt="소품대여" />
+        </S.Image>
+        <S.Content>
+          <S.Title>
+            <p>레트로 캐리어 가죽 여행가방 촬영소품</p>
+            <div className='heartBox'>
+              <FontAwesomeIcon icon={faHeart} className='heart'/>
+              <p>66</p>
+            </div>
+          </S.Title>
+          <p>25,000원</p>
+        </S.Content>
+      </S.Warpper>
+
+    </S.Container>
     </div>
   );
 };
