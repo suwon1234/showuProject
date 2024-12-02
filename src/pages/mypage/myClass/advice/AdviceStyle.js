eba75b1fef2187fa0d7ea9f6f3a001eb028b8fb6
minkyong00 <minkyong131@gmail.com>
2024-12-02 17:57:50 +0900

    myclass 상담신청내역 페이지 추가

--- src/pages/mypage/myClass/advice/AdviceStyle.js
+++ src/pages/mypage/myClass/advice/AdviceStyle.js
@@ -2,6 +2,31 @@ import styled from 'styled-components';
 
 const S = {};
 
-  
+  S.Table = styled.table`
+    margin-top: 40px;
+    width: 800px;
+    height: auto;
+    
+  `
+
+  S.Thead = styled.thead`
+    font-size: 17px;
+    background-color: #ffd400;
+    font-weight: 700;
+  `
+
+  S.Tr = styled.tr`
+    & th {
+      color: #333;
+      padding: 10px;
+    }
+  `
+
+  S.Tbody = styled.tbody`
+    & td {
+      padding: 20px 10px;
+    }
+  `
+
 
 export default S;
