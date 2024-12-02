adfd7917b3b5f8a4b7c6b1abfab6579b21423c6c
minkyong00 <minkyong131@gmail.com>
2024-12-03 01:34:45 +0900

    myActive 작성한 댓글 페이지 추가

--- src/pages/mypage/myActive/post/Comment.jsx
+++ src/pages/mypage/myActive/post/Comment.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import S from './CommentStyle';
 
 const Comment = () => {
   return (
     <div>
-      작성한 댓글 목록
+      <div>
+        <S.Table>
+          <S.Thead>
+            <S.Tr>
+              <th scope='col'></th>
+              <th scope='col'>댓글 내용</th>
+              <th scope='col'>카테고리</th>
+              <th scope='col'>작성일</th>
+            </S.Tr>
+          </S.Thead>
+          <S.Tbody>
+            <S.ContentTr>
+              <th scope='row'>1</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+            <S.ContentTr>
+              <th scope='row'>2</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+            <S.ContentTr>
+              <th scope='row'>3</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+            <S.ContentTr>
+              <th scope='row'>4</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+            <S.ContentTr>
+              <th scope='row'>5</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+            <S.ContentTr>
+              <th scope='row'>6</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+            <S.ContentTr>
+              <th scope='row'>7</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+            <S.ContentTr>
+              <th scope='row'>8</th>
+              <td>커뮤니티의 댓글의 내용이 들어가요</td>
+              <td>MD</td>
+              <td>2024.11.29 17:00</td>
+            </S.ContentTr>
+          </S.Tbody>
+        </S.Table>
+      </div>
     </div>
   );
 };
