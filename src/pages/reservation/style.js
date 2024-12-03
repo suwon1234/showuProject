fbc91ab3d91a771488b50c357a1fc35ca1aa0cc4
byungwook-dev <byungwook.dev@gmail.com>
2024-12-03 19:08:43 +0900

    예약 페이지 작업

--- src/pages/reservation/style.js
+++ src/pages/reservation/style.js
@@ -1,36 +1,150 @@
 import { styled } from "styled-components";
-import { backgroundBlack, flexCenter, flexCenterRow } from "../../global/common";
+import { backgroundBlack, flexCenterColumn } from "../../global/common";
 
 const S = {};
 
 // 전체 배경 색상
 S.MainContainer = styled.div`
   ${backgroundBlack}
-  ${flexCenterRow}
+  ${flexCenterColumn}
   width: 100%;
-  /* position: relative; */
   min-height: 100%;
 `;
 
-// 티켓오픈 컨테이너
+// 섹션 타이틀
+S.SectionTitle = styled.h2`
+  font-size: ${({ theme }) => theme.FONT_SIZE.h3};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+  color: #ffffff;
+  justify-content: center;
+  display: flex;
+  margin-top: 10px;
+  margin-bottom: 30px;
+  padding: 10px;
+`;
+
+// 티켓 오픈 컨테이너
 S.TicketOpen = styled.div`
   margin-top: 100px;
   width: 1200px;
-  height: 500px;
-  background-color: gray;
-`
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-// 티켓오픈 타이틀
- S.FirstTitle = styled.div`
-   font-size: ${({ theme }) => theme.FONT_SIZE.h3};
-   font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
-   color: #ffffff;
-   justify-content: center;
-   display: flex;
-   margin-top : 20px;
-   padding: 30px;
- `;
+// 티켓 박스 컨테이너
+S.TicketBoxContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+`;
 
+// 티켓 박스
+S.TicketBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 300px;
+  height: 200px;
+  margin: 5px;
+  margin-bottom: 150px;
+  background-color: white;
+  border-radius: 5px;
 
+  h3,
+  p {
+    margin: 5px 0;
+    color: white;
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.FONT_SIZE.h4};
+  }
+
+  p {
+    font-size: ${({ theme }) => theme.FONT_SIZE.body};
+  }
+`;
+
+// 현재 공연 중 컨테이너
+S.CurrentlyPerforming = styled.div`
+  margin-top: 50px;
+  width: 1200px;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+// 공연 박스 컨테이너
+S.PerformingBoxContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 200px;
+`;
+
+// 공연 박스
+S.PerformingBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 180px;
+  height: 220px;
+  margin: 10px;
+  background-color: white;
+  border-radius: 5px;
+
+  h3,
+  p {
+    margin: 5px 0;
+    color: white;
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.FONT_SIZE.h4};
+  }
+
+  p {
+    font-size: ${({ theme }) => theme.FONT_SIZE.body};
+  }
+`;
+
+// 티켓 오픈 이미지 스타일
+S.TicketImage = styled.img`
+  width: 300px;
+  height: 200px;
+  border-radius: 5px;
+`;
+
+// 현재 공연 중 이미지 스타일
+S.PerformingImage = styled.img`
+  width: 180px;
+  height: 400px;
+  border-radius: 5px;
+`;
+
+// 전체 보기 버튼
+S.ViewAllButton = styled.div`
+  border-radius: 30px;
+  background-color: #ffd400;
+  color: black;
+  width: 250px;
+  height: 50px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: ${({ theme }) => theme.FONT_SIZE.h5};
+  font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+  margin-top: 10px;
+  cursor: pointer;
+  margin-bottom: 20px;
+`;
 
 export default S;
--- src/routes/router.js
+++ src/routes/router.js
@@ -10,55 +10,64 @@ import ReservationContainer from '../pages/reservation/ReservationContainer';
 import ShopContainer from '../pages/shop/ShopContainer';
 import ShowuContainer from '../pages/showu/ShowuContainer';
 import VodContainer from '../pages/vod/VodContainer';
+import PerformingShow from '../pages/reservation/performing/PerformingShow';
+import TicketOpen from '../pages/reservation/ticket/TicketOpen';
 
 const router = createBrowserRouter([
   {
-    path : '/',
-    element : <Layout />,
-    children : [
+    path: "/",
+    element: <Layout />,
+    children: [
       {
-        index : true,
-        element : <Main />
+        index: true,
+        element: <Main />,
       },
       {
-        path : '/admin',
-        element : <AdminContainer />
+        path: "/admin",
+        element: <AdminContainer />,
       },
       {
-        path : '/community',
-        element : <CommunityContainer />
+        path: "/community",
+        element: <CommunityContainer />,
       },
       {
-        path : '/index',
-        element : <IndexContainer />
+        path: "/index",
+        element: <IndexContainer />,
       },
       {
-        path : '/login',
-        element : <LoginContainer />
+        path: "/login",
+        element: <LoginContainer />,
       },
       {
-        path : '/mypage',
-        element : <MypageContainer />
+        path: "/mypage",
+        element: <MypageContainer />,
       },
       {
-        path : '/reservation',
-        element : <ReservationContainer />
+        path: "/reservation",
+        element: <ReservationContainer />,
       },
       {
-        path : '/shop',
-        element : <ShopContainer />
+        path: "/reservation/performing-show",
+        element: <PerformingShow />,
       },
       {
-        path : '/showu',
-        element : <ShowuContainer />
+        path: "/reservation/ticket-open",
+        element: <TicketOpen />,
       },
       {
-        path : '/vod',
-        element : <VodContainer />
+        path: "/shop",
+        element: <ShopContainer />,
       },
-
-    ]
-  }
-])
+      {
+        path: "/showu",
+        element: <ShowuContainer />,
+      },
+      {
+        path: "/vod",
+        element: <VodContainer />,
+      },
+    ],
+  },
+]);
 
 export default router;
