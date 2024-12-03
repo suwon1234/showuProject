712406fef708686d820bc6ebabb1933c7a05c7d5
alsdk1676 <alsdk6761@gmail.com>
2024-12-03 19:03:34 +0900

    폴더 추가

---
+++ src/pages/shop/md/MdPayInfo/PayInfoContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MdPayInfo from './_component/MdPayInfo';
+
+const PayInfoContainer = () => {
+  return (
+    <div>
+      <MdPayInfo />
+    </div>
+  );
+};
+
+export default PayInfoContainer;
---
+++ src/pages/shop/md/MdReturn/ReturnContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MdReturn from './_component/MdReturn';
+
+const ReturnContainer = () => {
+  return (
+    <div>
+      <MdReturn />
+    </div>
+  );
+};
+
+export default ReturnContainer;
