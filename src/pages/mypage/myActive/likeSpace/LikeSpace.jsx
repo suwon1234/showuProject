90437e823ec460b41b1e58fc2d910980048aa415
minkyong00 <minkyong131@gmail.com>
2024-12-04 18:32:30 +0900

    myActive 찜 공간 페이지 추가

--- src/pages/mypage/myActive/likeSpace/LikeSpace.jsx
+++ src/pages/mypage/myActive/likeSpace/LikeSpace.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
+import S from './LikeSpaceStyle';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const LikeSpace = () => {
   return (
-    <div>
-      공간 찜 목록
-    </div>
+    <S.Container className='container'>
+      <S.Warpper className='warpper'>
+        <S.Image className='image'>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/space.png"} alt="공간대여" />
+        </S.Image>
+        <S.Content className='content'>
+          <S.Title className='title'>
+            <p>지스페이스홀(G-SPACEHALL)</p>
+          </S.Title>
+          <p>공연홀</p>
+          <p>서울 마포구</p>
+          <p>660,000원~ /8시간</p>
+          <FontAwesomeIcon icon={faHeart} className='heart'/>
+        </S.Content>
+      </S.Warpper>
+      <S.Warpper className='warpper'>
+        <S.Image className='image'>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/space.png"} alt="공간대여" />
+        </S.Image>
+        <S.Content className='content'>
+          <S.Title className='title'>
+            <p>지스페이스홀(G-SPACEHALL)</p>
+          </S.Title>
+          <p>공연홀</p>
+          <p>서울 마포구</p>
+          <p>660,000원~ /8시간</p>
+          <FontAwesomeIcon icon={faHeart} className='heart'/>
+        </S.Content>
+      </S.Warpper>
+      <S.Warpper className='warpper'>
+        <S.Image className='image'>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/space.png"} alt="공간대여" />
+        </S.Image>
+        <S.Content className='content'>
+          <S.Title className='title'>
+            <p>지스페이스홀(G-SPACEHALL)</p>
+          </S.Title>
+          <p>공연홀</p>
+          <p>서울 마포구</p>
+          <p>660,000원~ /8시간</p>
+          <FontAwesomeIcon icon={faHeart} className='heart'/>
+        </S.Content>
+      </S.Warpper>
+      <S.Warpper className='warpper'>
+        <S.Image className='image'>
+          <img src={process.env.PUBLIC_URL + "/images/Mypage/myRes/space.png"} alt="공간대여" />
+        </S.Image>
+        <S.Content className='content'>
+          <S.Title className='title'>
+            <p>지스페이스홀(G-SPACEHALL)</p>
+          </S.Title>
+          <p>공연홀</p>
+          <p>서울 마포구</p>
+          <p>660,000원~ /8시간</p>
+          <FontAwesomeIcon icon={faHeart} className='heart'/>
+        </S.Content>
+      </S.Warpper>
+
+    </S.Container>
   );
 };
 
