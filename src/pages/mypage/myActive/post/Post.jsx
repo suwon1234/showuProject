a962ee0dd7722da23b6dc81f11bb390e1238509c
minkyong00 <minkyong131@gmail.com>
2024-12-04 17:50:53 +0900

    myActive 작성한 글 페이지 추가

--- src/pages/mypage/myActive/post/Post.jsx
+++ src/pages/mypage/myActive/post/Post.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import S from './PostStyle';
 
 const Post = () => {
   return (
     <div>
-      <div className='Container'>
-        <div className='Wrapper'>
-          <div className='image'>
+      <S.Container className='Container'>
+        <S.Wrapper className='Wrapper'>
+          <S.Image className='image'>
             <img src={process.env.PUBLIC_URL + "/images/mypage/myActive/post.png"} alt="글 목록 이미지" />
-          </div>
-          <div className='Title'>
+          </S.Image>
+          <S.Title className='Title'>
             <p>커뮤니티에서 작성한 글의 제목</p>
             <p>커뮤니티에서 작성한 글의 작성자</p>
-          </div>
-        </div>
-      </div>
+          </S.Title>
+        </S.Wrapper>
+        <S.Wrapper className='Wrapper'>
+          <S.Image className='image'>
+            <img src={process.env.PUBLIC_URL + "/images/mypage/myActive/post.png"} alt="글 목록 이미지" />
+          </S.Image>
+          <S.Title className='Title'>
+            <p>커뮤니티에서 작성한 글의 제목</p>
+            <p>커뮤니티에서 작성한 글의 작성자</p>
+          </S.Title>
+        </S.Wrapper>
+        <S.Wrapper className='Wrapper'>
+          <S.Image className='image'>
+            <img src={process.env.PUBLIC_URL + "/images/mypage/myActive/post.png"} alt="글 목록 이미지" />
+          </S.Image>
+          <S.Title className='Title'>
+            <p>커뮤니티에서 작성한 글의 제목</p>
+            <p>커뮤니티에서 작성한 글의 작성자</p>
+          </S.Title>
+        </S.Wrapper>
+        <S.Wrapper className='Wrapper'>
+          <S.Image className='image'>
+            <img src={process.env.PUBLIC_URL + "/images/mypage/myActive/post.png"} alt="글 목록 이미지" />
+          </S.Image>
+          <S.Title className='Title'>
+            <p>커뮤니티에서 작성한 글의 제목</p>
+            <p>커뮤니티에서 작성한 글의 작성자</p>
+          </S.Title>
+        </S.Wrapper>
+      </S.Container>
     </div>
   );
 };
--- src/pages/mypage/myActive/post/PostStyle.js
+++ src/pages/mypage/myActive/post/PostStyle.js
@@ -4,17 +4,40 @@ const S = {};
 
   S.Container = styled.div`
     width: 900px;
-    height: 100vh;
+    height: 100%;
+
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: 5%;
+    row-gap: 30px;
   `
 
   S.Wrapper = styled.div`
     width: 360px;
-    height: 340px;
+    height: 320px;
   `
 
   S.Image = styled.div`
     width: 360px;
-    height: 340px;
+    height: 280px;
+
+    & img {
+      border-radius: 4px;
+    }
+  `
+
+  S.Title = styled.div`
+
+    & p:first-child {
+      font-size: 17px;
+      font-weight: 700;
+      margin-top: 8px;
+    }
+
+    & p:last-child {
+      font-size: 15px;
+      font-weight: 400;
+    }
   `
 
 export default S;
