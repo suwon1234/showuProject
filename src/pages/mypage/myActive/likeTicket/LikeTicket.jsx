08f0c550b56efd1330774044bc24abc5584a1fd2
minkyong00 <minkyong131@gmail.com>
2024-12-04 19:11:21 +0900

    myActive 찜 티켓 페이지

--- src/pages/mypage/myActive/likeTicket/LikeTicket.jsx
+++ src/pages/mypage/myActive/likeTicket/LikeTicket.jsx
@@ -2,8 +2,34 @@ import React from 'react';
 
 const LikeTicket = () => {
   return (
-    <div>
-      티켓 찜 목록
+    <div className='Container'>
+      <div className='Wrapper'>
+        <div className='ticketBox'>
+          <p>뮤지컬 &lt;클로버&gt;</p>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th scope='col'>예매번호</th>
+                <th scope='col'>관람일시</th>
+                <th scope='col'>티켓</th>
+                <th scope='col'>결제금액</th>
+                <th scope='col'>취소가능일</th>
+                <th scope='col'>현재상태</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope='row'>1</th>
+                <td>2024.11.30(금) 13:00</td>
+                <td>1매</td>
+                <td>66,000원</td>
+                <td>2024.11.29 19:30</td>
+                <td>예매완료</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/mypage/myActive/likeTicket/LikeTicketStyle.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const S = {};
+
+  S.Conatiner = styled.div`
+    width: 900px;
+    height: auto;
+  `
+
+  S.Wrapper = styled.div`
+    width: 900px;
+    height: 100%;
+  `
+
+  S.TicketBox = styled.div`
+    width: 400px;
+    height: 154px;
+  `
+
+
+export default S;
