e0c4d993d0993c45e9c46797024be07220ac585d
minkyong00 <minkyong131@gmail.com>
2024-12-16 21:27:28 +0900

    myActive review fetch, style 변경

--- src/pages/mypage/myActive/review/Review.jsx
+++ src/pages/mypage/myActive/review/Review.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import S from './ReviewStyle';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 const Review = () => {
+  const [ reviews, setReviews ] = useState([]);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      try {
+        const response = await fetch(`http://localhost:4000/review`);
+        const datas = await response.json();
+        setReviews(datas)
+      } catch (error) {
+        console.log("ReviewError", error)
+      }
+    }
+
+    getReviews()
+
+  }, [])
+
+  // console.log(reviews)
+
   return (
-    <div>
-      <div>
+    <S.Container className='Container'>
+      { reviews && reviews.map((item, i) => (
+        <S.Wrapper key={i} className='Wrapper'>
+          <S.Title className='title'>
+            <img src={item.profileImageUrl} alt="프로필 이미지" />
+            <S.RightBox className='rightBox'>
+              <p className='productName'>{item.productName}</p>
+              <p className='writer'>{item.writer}</p>
+            <S.Rating className='rating'>
+              <FontAwesomeIcon className='star' icon={faStar} />
+              <FontAwesomeIcon className='star' icon={faStar} />
+              <FontAwesomeIcon className='star' icon={faStar} />
+              <FontAwesomeIcon className='star' icon={faStar} />
+              <FontAwesomeIcon className='star' icon={faStar} />
+              <p className='date'>{item.date}</p>
+            </S.Rating>
+            </S.RightBox>
+          </S.Title>
+            <p className='content'>{item.content}</p>
+        </S.Wrapper>
+      ))}
+
+
+
+
+
+      {/* <div>
         <S.Table>
           <S.Thead>
             <S.Tr>
@@ -116,8 +160,8 @@ const Review = () => {
             </S.ContentTr>
           </S.Tbody>
         </S.Table>
-      </div>
-    </div>
+      </div> */}
+    </S.Container>
   );
 };
 
