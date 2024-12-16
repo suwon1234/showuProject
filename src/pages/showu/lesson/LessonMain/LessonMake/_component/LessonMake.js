50e2c8a47a0da636d806424f26e95d91e27ca596
seungyeoby <ysy9536@gmail.com>
2024-12-17 02:06:15 +0900

    241217_v1

--- src/pages/showu/lesson/LessonMain/LessonMake/_component/LessonMake.js
+++ src/pages/showu/lesson/LessonMain/LessonMake/_component/LessonMake.js
@@ -1,85 +1,90 @@
 import React from 'react';
+import S from '../../style';
 
 const LessonMake = () => {
     return (
-        <div>
-            <div className='LessonMakeTopBg'></div>
-            <img  className='LessonMakeThumbnail'/>
-            <div className='LessonMakingFormWrapper'>
-                <h1 className='LessonMakingTitle'>
+        <S.LessonMakeWrapper>
+            <S.LessonMakeTopBg></S.LessonMakeTopBg>
+            <S.LessonMakeThumbnail/>
+            <S.LessonMakingFormWrapper>
+                <S.LessonMakingTitle>
                     ShowU 레슨 등록하기
-                </h1>
+                </S.LessonMakingTitle>
                 <form className='LessonMakingForm'>
                     <div className='LessonProfileWrapper'>
-                        <h3 className='Title'>프로필 등록</h3>
-                        <div className='ProfileRegistration'>
-                            <img className='ProfileThumbnail' alt='프로필 썸네일'/>
-                            <div className='ProfileInputWrapper'>
-                                <input  type='text' placeholder='전문가 이름을 등록하세요'/>
+                        <S.MakeTitle>프로필 등록</S.MakeTitle>
+                        <S.ProfileRegistration>
+                            <S.ProfileThumbnail alt='프로필 썸네일'/>
+                            <S.ProfileInputWrapper>
+                                <input  type='text'     placeholder='전문가 이름을 등록하세요'/>
                                 <input  type='text' placeholder='레슨 이름을 등록 하세요'/>
                                 <input  type='text' placeholder='연락 가능시간'/>
-                            </div>   
-                        </div>
+                            </S.ProfileInputWrapper>   
+                        </S.ProfileRegistration>
                     </div>
 
-                    <div className='CategoryRegistration'>
-                        <h3 className='Title'>분야 등록</h3>
-                        <div className='LessonCategoryWrapper'>
-                            <input type='button' className='LessonCategory'><span>음악</span></input>
-                            <input type='button' className='LessonCategory'><span>연기</span></input>
-                            <input type='button' className='LessonCategory'><span>마술</span></input>
-                        </div>
-                    </div>
+                    <S.CategoryRegistration>
+                        <S.MakeTitle>분야 등록</S.MakeTitle>
+                        <S.LessonCategoryWrapper>
+                            <S.LessonCategory>음악</S.LessonCategory>
+                            <S.LessonCategory className='active'>연기</S.LessonCategory>
+                            <S.LessonCategory>미술</S.LessonCategory> 
+                        </S.LessonCategoryWrapper>
+                    </S.CategoryRegistration>
 
-                    <div className='LocationRegistation'>
-                        <h3 className='Title'>위치</h3>
-                        <input type='text' className='LessonLocation' placeholder='주소를 입력해 주세요'/>
-                        <input type='text' className='LessonLocation' placeholder='상세 주소를 입력해 주세요'/>
-                    </div>
+                    <S.LocationRegistation>
+                        <S.MakeTitle>위치</S.MakeTitle>
+                        <S.LessonLocationWrapper>
+                            <S.LessonLocation placeholder='주소를 입력해 주세요'/>
+                            <S.LessonLocation placeholder='상세 주소를 입력해 주세요'/>
+                        </S.LessonLocationWrapper>
+                    </S.LocationRegistation>
 
-                    <div className='DetailsRegistration'>
-                        <h3 className='Title'>레슨 상세설명</h3>
-                        <textarea className='LessonDetails' placeholder='상세설명을 적어주세요'/>
-                    </div>
+                    <S.DetailsRegistration>
+                        <S.MakeTitle>레슨 상세설명</S.MakeTitle>
+                        <S.LessonDetails placeholder='상세설명을 적어주세요'/>
+                    </S.DetailsRegistration>
 
-                    <div className='LessonEtcRegistration'>
-                        <h3 className='Title'>경력 / 이력 / 학력</h3>
-                        <div className='EtcWrapper'>
-                            <textarea className='EtcDetails' placeholder='경력 사항을 입력해 주세요'/>
-                            <textarea className='EtcDetails' placeholder='이력 사항을 입력해 주세요'/>
-                            <textarea className='EtcDetails' placeholder='학력 사항을 입력해 주세요'/>
-                        </div>
-                    </div>
+                    <S.LessonEtcRegistration>
+                        <S.MakeTitle>경력 / 이력 / 학력</S.MakeTitle>
+                        <S.EtcWrapper>
+                            <S.EtcDetails placeholder='경력 사항을 입력해 주세요'/>
+                            <S.EtcDetails placeholder='이력 사항을 입력해 주세요'/>
+                            <S.EtcDetails placeholder='학력 사항을 입력해 주세요'/>
+                        </S.EtcWrapper>
+                    </S.LessonEtcRegistration>
 
-                    <div className='PortfoilosRegistration'>
-                        <h3 className='Title'>포트폴리오</h3>
-                        <button className="PortfoilosRegistrationButton" >등록하기</button>
-                        <div>
-                            <img  className='PortfolioThumbnail'/>
-                            <img  className='PortfolioThumbnail'/>
-                        </div>
-                    </div>
+                    <S.PortfoilosRegistration>
+                        <S.MakeTitle>포트폴리오</S.MakeTitle>
+                        <S.PortfoilosRegistrationButton >등록하기</S.PortfoilosRegistrationButton>
+                        <S.PortfolioThumbnailWrapper>
+                            <S.PortfolioThumbnail/>
+                            <S.PortfolioThumbnail/>
+                        </S.PortfolioThumbnailWrapper>
+                    </S.PortfoilosRegistration>
 
-                    <div className='MediasRegistration'>
-                        <h3 className='Title'>동영상</h3>
-                        <button className="MediasRegistrationButton" >등록하기</button>
-                        <div>
-                            <img  className='MediasThumbnail'/>
-                            <img  className='MediasThumbnail'/>
-                        </div>
-                    </div>
+                    <S.MediasRegistration>
+                        <S.MakeTitle>동영상</S.MakeTitle>
+                        <S.MediasRegistrationButton>등록하기</S.MediasRegistrationButton>
+                        <S.MediasThumbnailWrapper>
+                            <S.MediasThumbnail/>
+                            <S.MediasThumbnail/>
+                        </S.MediasThumbnailWrapper>
+                    </S.MediasRegistration>
 
-                    <div className='LessonNoticeRegistration'>
-                        <h3 className='Title'>기타 공지사항</h3>
-                        <textarea  placeholder='기타 공지사항을 적어주세요'/>
-                    </div>
+                    <S.LessonNoticeRegistration>
+                        <S.MakeTitle>기타 공지사항</S.MakeTitle>
+                        <S.LessonNotice placeholder='기타 공지사항을 적어주세요'/>
+                    </S.LessonNoticeRegistration>
+                    <S.ButtonWrapper>
+                        <S.CancleButton>취소하기</S.CancleButton>
+                        <S.SubmitButton>등록 신청 하기</S.SubmitButton>
 
-                    <button className='CancleButton'>취소하기</button>
-                    <button type='submit' className='SubmitButton'>등록하기</button>
+                    </S.ButtonWrapper>
                 </form>
                 
-            </div>
-        </div>
+            </S.LessonMakingFormWrapper>
+        </S.LessonMakeWrapper>
     );
 };
 
