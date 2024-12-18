cd3ebfbd3d1793e7ce745c0569fbddba99217318
minkyong00 <minkyong131@gmail.com>
2024-12-19 00:35:29 +0900

    join 유효성 검사, 전체 동의 기능 추가

--- src/pages/join/style.js
+++ src/pages/join/style.js
@@ -4,7 +4,7 @@ const S = {};
 
   S.JoinBox = styled.div`
     width: 560px;
-    height: 890px;
+    height: 830px;
     margin: auto;
     padding: 40px 0;
     background-color: #252525;
@@ -36,11 +36,12 @@ const S = {};
   S.Form = styled.form`
     padding: 0 40px 0 40px;
     position: relative;
-  `
+    `
 
   S.idLabel = styled.label`
     display: block;
-    margin: 0 0 30px 0;
+    margin: 0 0 20px 0;
+    position: relative;
   `
   S.passwordLabel = styled.label`
     display: block;
@@ -50,8 +51,8 @@ const S = {};
 
   S.LockImage = styled.img`
     position: absolute;
-    top: 100px;
-    left: 475px;
+    top: 17px;
+    right: 20px;
 
     cursor: pointer;
   `
@@ -83,32 +84,16 @@ const S = {};
       color: black;
     }
   `
+  S.JoinButton = styled.button`
+    width: 480px;
+    height: 55px;
+    margin: 20px 40px 0 40px;
 
-  S.AgreeWapper = styled.ul`
-    margin: 0 40px;
+    background-color: ${(props) => props.state ? "#ffd400" : "#717171" };
+    color: #fff;
+    border: none;
+    border-radius: 50px;
+    font-size: 17px;
   `
-
-  S.AgreeBox = styled.div`
-    display: flex;
-    align-items: center;
-    margin-bottom: 5px;
-
-    & .AllAgree {
-      font-size: 16px;
-      font-weight: 700;
-    }
-
-    & span {
-      font-size: 15px;
-      font-weight: 400;
-      margin-left: 5px;
-    }
-  `
-
-  S.CheckImg = styled.img`
-    margin-right: 10px;
-  `
-  
-
 
 export default S;
