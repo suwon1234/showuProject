726a6f5ece022b216d14f467c0d8aad1811d0c20
jiihyun12 <j22h.h0h@gmail.com>
2024-12-19 21:45:08 +0900

    체크아웃 수정

--- src/pages/login/_component/Checkbox.js
+++ src/pages/login/_component/Checkbox.js
@@ -1,101 +1,101 @@
-import React, { useEffect, useState } from 'react';
-import S from '../_component/CheckboxStyle';
+// import React, { useEffect, useState } from 'react';
+// import S from '../_component/CheckboxStyle';
 
-const Checkbox = ({ setButtonColor }) => {
-  const [checkList, setCheckList] = useState([]);
+// const Checkbox = ({ setButtonColor }) => {
+//   const [checkList, setCheckList] = useState([]);
 
-  const checkAll = (e) => {
-    e.target.checked
-      ? setCheckList(["term1", "term2", "term3", "term4"])
-      : setCheckList([]);
-  };
+//   const checkAll = (e) => {
+//     e.target.checked
+//       ? setCheckList(["term1", "term2", "term3", "term4"])
+//       : setCheckList([]);
+//   };
 
-  const check = (e) => {
-    e.target.checked
-      ? setCheckList([...checkList, e.target.name])
-      : setCheckList(checkList.filter((choice) => choice !== e.target.name));
-  };
+//   const check = (e) => {
+//     e.target.checked
+//       ? setCheckList([...checkList, e.target.name])
+//       : setCheckList(checkList.filter((choice) => choice !== e.target.name));
+//   };
 
-  useEffect(() => {
-    const allChecked = checkList.includes("term1") &&
-      checkList.includes("term2") &&
-      checkList.includes("term3")
-    setButtonColor(allChecked); 
-  }, [checkList, setButtonColor]);
+//   useEffect(() => {
+//     const allChecked = checkList.includes("term1") &&
+//       checkList.includes("term2") &&
+//       checkList.includes("term3")
+//     setButtonColor(allChecked); 
+//   }, [checkList, setButtonColor]);
 
-  return (
-    <S.AgreeWapper>
-      <S.AgreeBox>
-        <S.Checkbox>
-          <input
-            type="checkbox"
-            id="check"
-            name="all"
-            onChange={checkAll}
-            checked={checkList.length === 4}
-          />
-          <label htmlFor="check"></label>
-        </S.Checkbox>
-        <span className="AllAgree">필수 및 선택 항목을 모두 포함하여 동의합니다.</span>
-      </S.AgreeBox>
+//   return (
+//     <S.AgreeWapper>
+//       <S.AgreeBox>
+//         <S.Checkbox>
+//           <input
+//             type="checkbox"
+//             id="check"
+//             name="all"
+//             onChange={checkAll}
+//             checked={checkList.length === 4}
+//           />
+//           <label htmlFor="check"></label>
+//         </S.Checkbox>
+//         <span className="AllAgree">필수 및 선택 항목을 모두 포함하여 동의합니다.</span>
+//       </S.AgreeBox>
 
-      <S.AgreeBox>
-        <S.Checkbox>
-          <input
-            type="checkbox"
-            id="check"
-            name="term1"
-            onChange={check}
-            checked={checkList.includes("term1")}
-          />
-          <label htmlFor="check"></label>
-        </S.Checkbox>
-        <span>만 14세 이상입니다.</span>
-      </S.AgreeBox>
+//       <S.AgreeBox>
+//         <S.Checkbox>
+//           <input
+//             type="checkbox"
+//             id="check"
+//             name="term1"
+//             onChange={check}
+//             checked={checkList.includes("term1")}
+//           />
+//           <label htmlFor="check"></label>
+//         </S.Checkbox>
+//         <span>만 14세 이상입니다.</span>
+//       </S.AgreeBox>
 
-      <S.AgreeBox>
-        <S.Checkbox>
-          <input
-            type="checkbox"
-            id="check"
-            name="term2"
-            onChange={check}
-            checked={checkList.includes("term2")}
-          />
-          <label htmlFor="check"></label>
-        </S.Checkbox>
-        <span>[필수] 서비스 약관 동의</span>
-      </S.AgreeBox>
+//       <S.AgreeBox>
+//         <S.Checkbox>
+//           <input
+//             type="checkbox"
+//             id="check"
+//             name="term2"
+//             onChange={check}
+//             checked={checkList.includes("term2")}
+//           />
+//           <label htmlFor="check"></label>
+//         </S.Checkbox>
+//         <span>[필수] 서비스 약관 동의</span>
+//       </S.AgreeBox>
 
-      <S.AgreeBox>
-        <S.Checkbox>
-          <input
-            type="checkbox"
-            id="check"
-            name="term3"
-            onChange={check}
-            checked={checkList.includes("term3")}
-          />
-          <label htmlFor="check"></label>
-        </S.Checkbox>
-        <span>[필수] 개인정보 수집 및 이용 동의</span>
-      </S.AgreeBox>
+//       <S.AgreeBox>
+//         <S.Checkbox>
+//           <input
+//             type="checkbox"
+//             id="check"
+//             name="term3"
+//             onChange={check}
+//             checked={checkList.includes("term3")}
+//           />
+//           <label htmlFor="check"></label>
+//         </S.Checkbox>
+//         <span>[필수] 개인정보 수집 및 이용 동의</span>
+//       </S.AgreeBox>
 
-      <S.AgreeBox>
-        <S.Checkbox>
-          <input
-            type="checkbox"
-            id="check"
-            name="term4"
-            onChange={check}
-            checked={checkList.includes("term4")}
-          />
-          <label htmlFor="check"></label>
-        </S.Checkbox>
-        <span>[선택] 개인정보 수집 및 이용 동의</span>
-      </S.AgreeBox>
-    </S.AgreeWapper>
-  );
-};
+//       <S.AgreeBox>
+//         <S.Checkbox>
+//           <input
+//             type="checkbox"
+//             id="check"
+//             name="term4"
+//             onChange={check}
+//             checked={checkList.includes("term4")}
+//           />
+//           <label htmlFor="check"></label>
+//         </S.Checkbox>
+//         <span>[선택] 개인정보 수집 및 이용 동의</span>
+//       </S.AgreeBox>
+//     </S.AgreeWapper>
+//   );
+// };
 
-export default Checkbox;
+// export default Checkbox;
