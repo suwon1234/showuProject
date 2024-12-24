d40207d60aecc5b0517ae62da0967186068e2125
minkyong00 <minkyong131@gmail.com>
2024-12-24 17:24:14 +0900

    헤더 style 수정

--- src/pages/layout/Layout.js
+++ src/pages/layout/Layout.js
@@ -1,39 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { Link, NavLink, Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import S from './style';
 import Footer from './footer/Footer';
 import { setUser, setUserStatus } from '../../modules/user';
 import HoverMenu from './_component/HoverMenu';
 
 const Layout = () => {
+
   const { isLogin, currentUser } = useSelector((state) => state.user)
   const dispatch = useDispatch();
   const [searchParams] = useSearchParams();
-  const [jwtToken, setJwtToken] = useState("");
   const navigate = useNavigate();
+
   const handleLogout = () => {
     localStorage.removeItem("jwtToken") //토큰 삭제
     dispatch(setUser({})) //리덕스 초기화
     dispatch(setUserStatus(false))
     console.log("로그아웃")
   }
-
-  const [hover, setHover] = useState(false);  // 커뮤니티 호버 상태
-    const handleMouseEnter = () => {
-      setHover(true);
-  } 
-    const handleMouseLeave = () => {
-      setHover(false);
-  };
-
-    const [hover2, setHover2] = useState(false);  // Shop 호버 상태
-    const handleMouseEnter2 = () => {
-      setHover2(true);
-  } 
-    const handleMouseLeave2 = () => {
-      setHover2(false);
-  };
 
   const location = useLocation();
 
@@ -76,112 +63,124 @@ const Layout = () => {
 
   }, [searchParams, dispatch, navigate]);
 
-    
-
   return (
     <div>
       <S.Background className='Background'>
-        { !hideHeaderPage.includes(location.pathname) && <header className="header">
-        <S.topbar className="topbar"></S.topbar>
-        <S.navbar className="navbar">
-      <S.logo className="logo">
-        <Link to="/main" className="logolink">
-          Show <span className="highlight">U</span>
-        </Link>
-      </S.logo>
-      <S.searchinput
-        type="text"
-        placeholder="너의 재능을 보여줘"
-        className="searchinput"
-      />
-      <S.authlinks className="authlinks">
-        <Link to="/up-grade" className="highlight">
-          등급업 신청
-        </Link>
-        <span className="divider">|</span>
-        { isLogin ? (
-          <>
-            <span>{currentUser.email}</span>
-            <button onClick={handleLogout}>로그아웃</button>
-          </>
-        ) : (
-          <>
-          <Link to="/login" className="highlight">
-            로그인
-          </Link>
-          <Link to="/join" className="showUlink">
-            회원가입
-          </Link>
-        </>
-        )
-        }
-      </S.authlinks>
-        </S.navbar>
-        <S.menubar className="menubar">
+        { !hideHeaderPage.includes(location.pathname) && 
+          <header className="header">
+            <S.topbar className="topbar"></S.topbar>
 
-        {/* showU  */}
-        <HoverMenu 
-        menuLabel="showU"
-        to="/showu"
-        dropdownLinks={[
-          {to: "/showu", label: "팀매칭"},
-          {to: "/showu", label: "레슨"}
-        ]}
-      />
-      
-      {/* Shop */}
-      <HoverMenu 
-        menuLabel="Shop"
-        to="/shop"
-        dropdownLinks={[
-          { to: "/shop/md", label: "MD" },
-          { to: "/shop/auction", label: "경매" },
-        ]}
-      />
+              <S.navbar className="navbar">
+                <S.LogoBox className='logoBox'>
+                  <S.logo className="logo">
+                    <Link to="/main" className="logolink">
+                      Show <span className="highlight">U</span>
+                    </Link>
+                  </S.logo>
 
-      <NavLink to={"/vod"} className="menuitem">
-        VOD
-      </NavLink>
+                  <S.SearchBox className='searchBox'>
+                    <input
+                      type="text"
+                      placeholder="너의 재능을 보여줘"
+                      className="searchinput"
+                    />
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className='search'/>
+                  </S.SearchBox>
 
-      {/* 예약 */}
-      <HoverMenu 
-        menuLabel="예약"
-        to="/reservation"
-        dropdownLinks={[
-          {to: "/reservation", label: "공간 대여"},
-          {to: "/reservation", label: "티켓 예매"}
-        ]}
-      />
+                </S.LogoBox>
 
-      <span className="menudivider">|</span>
-      
-      <NavLink to={"/hot"} className="menuitemhot">
-        HOT
-      </NavLink>
-      
-      {/* 커뮤니티 */}
-      <HoverMenu 
-        menuLabel="커뮤니티"
-        to="/community"
-        dropdownLinks={[
-          { to: "/community/newsMain", label: "News" },
-          { to: "/community/audition", label: "Audition" },
-        ]}
-      />
-      <NavLink to={"/mypage"} className="menuitem">
-        마이페이지
-      </NavLink>
-        </S.menubar>
-      </header>}
-      <S.main className='main'>
-        <Outlet/>
-      </S.main>
+              <S.authlinks className="authlinks">
+                <Link to="/up-grade" className="highlight">
+                  등급업 신청
+                </Link>
+                <span className="divider">|</span>
+
+                { isLogin ? (
+                  <>
+                    <span>{currentUser.email}</span>
+                    <button onClick={handleLogout}>로그아웃</button>
+                  </>
+                ) : (
+                  <>
+                  <Link to="/login" className="highlight">
+                    로그인
+                  </Link>
+                  <Link to="/join" className="showUlink">
+                    회원가입
+                  </Link>
+                </>
+                )}
+
+              </S.authlinks>
+              </S.navbar>
+
+              <S.menubar className="menubar">
+
+              {/* showU  */}
+              <HoverMenu 
+                menuLabel="showU"
+                to="/showu"
+                dropdownLinks={[
+                  {to: "/showu", label: "팀매칭"},
+                  {to: "/showu", label: "레슨"}
+                ]}
+              />  
+        
+              {/* Shop */}
+              <HoverMenu 
+                menuLabel="Shop"
+                to="/shop"
+                dropdownLinks={[
+                  { to: "/shop/md", label: "MD" },
+                  { to: "/shop/auction", label: "경매" },
+                ]}
+              />
+
+              <NavLink to={"/vod"} className="menuitem">
+                VOD
+              </NavLink>
+
+              {/* 예약 */}
+              <HoverMenu 
+                menuLabel="예약"
+                to="/reservation"
+                dropdownLinks={[
+                  {to: "/reservation", label: "공간 대여"},
+                  {to: "/reservation", label: "티켓 예매"}
+                ]}
+              />
+
+              <span className="menudivider">|</span>
+            
+              <NavLink to={"/hot"} className="menuitemhot">
+                HOT
+              </NavLink>
+        
+              {/* 커뮤니티 */}
+              <HoverMenu 
+                menuLabel="커뮤니티"
+                to="/community"
+                dropdownLinks={[
+                  { to: "/community/newsMain", label: "News" },
+                  { to: "/community/audition", label: "Audition" },
+                ]}
+              />
+
+              <NavLink to={"/mypage"} className="menuitem">
+                마이페이지
+              </NavLink>
+            </S.menubar>
+          </header> }
+
+        {/* 메인 */}
+        <S.main className='main'>
+          <Outlet/>
+        </S.main>
       </S.Background>
-      
+            
+      {/* 푸터 */}
       <Footer />
     </div>
-
-    
   );
 };
 
--- src/pages/layout/style.js
+++ src/pages/layout/style.js
@@ -2,153 +2,185 @@ import styled from 'styled-components';
 
 const S = {}
 
-S.Background=styled.div`
-  width: 100%;
-  position: relative;
-  min-height: 100%;
-  background-color: black;
-`
-S.header=styled.header`
-min-width: 1240px;
-background-color: black;
-padding: 10px 20px;
-position: sticky;
-`
+  S.Background=styled.div`
+    width: 100%;
+    position: relative;
+    min-height: 100%;
+    background-color: black;
+  `
 
-S.topbar=styled.div`
-  width: 100%;
-  height: 48px;
-  background-color: #FDDE39;
-`
-S.navbar=styled.nav`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 10px 20px;
-`
-S.logo=styled.div`
-  color: white;
-  font-size: 24px;
-  font-weight: bold;
- 
+  S.header=styled.header`
+    min-width: 1240px;
+    background-color: black;
+    padding: 10px 20px;
+    position: sticky;
+  `
 
-  & .logolink{
-    font-size: 1.5rem;
+  S.topbar=styled.div`
+    width: 100%;
+    height: 48px;
+    background-color: #FDDE39;
+  `
+
+  S.navbar=styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 10px 20px;
+    gap: 100px;
+  `
+
+  S.LogoBox = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 90px;
+  `
+
+  S.logo=styled.div`
     color: white;
-    text-decoration: none;
-    font-style: Pretendard;
-  }
-  & span {
-    color: #FDDE39;
+    font-size: 24px;
+    font-weight: bold;
+  
+
+    & .logolink{
+      font-size: 1.5rem;
+      color: white;
+      text-decoration: none;
+      font-style: Pretendard;
+    }
+
+    & span {
+      color: #FDDE40;
+      
+    }
+
+  `
+
+  S.SearchBox = styled.div`
+    width: 549px;
+    height: 54px;
+    position: relative;
     
-  }
+    & input {
+      width: 100%;
+      height: 100%;
+      border-radius: 50px;
+      border: 1px solid #444444;
+      padding: 10px 8px 8px 15px;
+      position: relative;
 
-`
-
-S.searchinput=styled.input`
-  margin-left: 10px;
-  padding: 8px;
-  font-size: 1rem;
-  border: none;
-  border-radius: 50px;
-  width: 549px;
-  height: 54px;
-  border: 1px solid #ccc;
-
-`
-
-
-S.authlinks=styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  font-size: 14px;
-
-  & .highlight{
-    color: #FDDE39;
-  }
-
-  & .showUlink{
-    color: #717171;
-  }
-  & span {
-    color: #FDDE39;
-    
-  }
-
-`
-
-S.menubar=styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 18px;
-  gap: 84px;
-
-  & .menuitem{
-  text-decoration: none;
-  color: white;
-  font-size: 18px;
-  & .active {
-      & p {
-        color : #917cf0 !important;
+      &::placeholder {
+        font-size: 20px;
       }
+    }
+
+    & svg.search {
+      position: absolute;
+      top: 15px;
+      right: 15px;
+      width: 20px;
+      height: 20px;
 
       & path {
-        color : #917cf0 !important;
+        color: #000;
       }
     }
-  }
-  & .menuitemhot{
-    color: red;
-  }
-  & .menudivider{
-    color: white;
-  } 
+  `
 
-`
-S.main=styled.main`
-width: 100%;
-z-index: 10;
-margin: 0 auto;
-padding-bottom: 50px;
-height: 100%;
-min-height: 1080px;
-overflow: hidden;
-min-width: 1240px;
-background-color: black;
+  S.authlinks=styled.div`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: 14px;
 
-& .slider{
-
-}
-`
-
-
-/* 커뮤니티 메뉴 스타일 */
-
-S.dropdown = styled.div`
-  position: absolute;
-  background-color: black;
-  border: 1px solid #ccc;
-  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
-  z-index: 10;
-  display: block;
-`;
-
-S.dropdownItem = styled.div`
-padding: 10px 20px;
-
-  a {
-    color: white;
-    text-decoration: none;
-    display: block;
-    padding: 5px 0;
-  }
-
-  a:hover {
-    color: #FDDE39;
-    font-weight: bold;
-    text-decoration: underline;
+    & .highlight{
+      color: #FDDE39;
     }
+
+    & .showUlink{
+      color: #717171;
+    }
+    & span {
+      color: #FDDE39;
+      
+    }
+
+  `
+
+  S.menubar=styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: 18px;
+    gap: 84px;
+
+    & .menuitem{
+    text-decoration: none;
+    color: white;
+    font-size: 18px;
+
+    & .active {
+        & p {
+          color : #917cf0 !important;
+        }
+
+        & path {
+          color : #917cf0 !important;
+        }
+      }
+    }
+
+    & .menuitemhot{
+      color: red;
+    }
+
+    & .menudivider{
+      color: white;
+    } 
+
+  `
+  S.main=styled.main`
+    width: 100%;
+    z-index: 10;
+    margin: 0 auto;
+    padding-bottom: 50px;
+    height: 100%;
+    min-height: 1080px;
+    overflow: hidden;
+    min-width: 1240px;
+    background-color: black;
+
+    & .slider{
+
+    }
+  `
+
+
+  /* 커뮤니티 메뉴 스타일 */
+
+  S.dropdown = styled.div`
+    position: absolute;
+    background-color: black;
+    border: 1px solid #ccc;
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+    z-index: 10;
+    display: block;
+  `;
+
+  S.dropdownItem = styled.div`
+    padding: 10px 20px;
+
+      a {
+        color: white;
+        text-decoration: none;
+        display: block;
+        padding: 5px 0;
+      }
+
+      a:hover {
+        color: #FDDE39;
+        font-weight: bold;
+        text-decoration: underline;
+        }
   `;
 
 
