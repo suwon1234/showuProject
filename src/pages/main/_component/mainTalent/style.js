ba4e2a06dbd952a679161794b25a0abfbe8e5ee1
jiihyun12 <j22h.h0h@gmail.com>
2024-12-26 17:28:25 +0900

    수정 저장

--- src/pages/main/_component/mainTalent/style.js
+++ src/pages/main/_component/mainTalent/style.js
@@ -3,6 +3,14 @@ import styled from 'styled-components';
 const S = {}; 
 
 S.Wrapper = styled`
+gap: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100%;
+    height: 1080px;
+    background-color: #ffd400;
+    padding-top: 40px;
 
 `
 
