ffb3c44b28031c32bd9bb5ba0d324345313fbb37
minkyong00 <minkyong131@gmail.com>
2024-12-29 16:41:13 +0900

    자주 묻는 질문 수정중

--- src/pages/FAQ/FAQContainer.js
+++ src/pages/FAQ/FAQContainer.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import useDropdown from '../../hooks/useDropdown';
+import FaqList from './FaqList';
 
 const FAQContainer = () => {
+  const dropdownRef = useRef(null);
+  const [ isOpen, setIsOpen ] = useDropdown(dropdownRef, false);
+
   return (
-    <div>
-      자주 묻는 질문
+    <div className='Container'>
+      <div className='fqaTitle'>
+        <p>자주 묻는 질문(FAQ)</p>
+      </div>
+      <div>
+        <FaqList isOpen={isOpen} setIsOpen={setIsOpen} />
+      </div>
     </div>
   );
 };
