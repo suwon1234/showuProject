307915d7325d7de97d40bb93681f6a0aa7875068
minkyong00 <minkyong131@gmail.com>
2025-01-03 02:30:06 +0900

    마이페이지 찜한 md 조회 추가

--- src/pages/mypage/myActive/likeMd/LikeMd.jsx
+++ src/pages/mypage/myActive/likeMd/LikeMd.jsx
@@ -2,16 +2,32 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
 import S from './LikeMdStyle';
+import { useSelector } from 'react-redux';
 
 const LikeMd = () => {
   const [ mds, setMds ] = useState([]);
+  const { currentUser } = useSelector((state) => state.user);
+  const userId = currentUser ? currentUser._id : '';
+  const jwtToken = localStorage.getItem("jwtToken");
 
   useEffect(() => {
     const getMds = async () => {
+      if(!userId || !jwtToken) return;
+
       try {
-        const response = await fetch(`http://localhost:4000/md`);
-        const datas = await response.json();
-        setMds(datas)
+        const response = await fetch(`http://localhost:8000/my/like/md`, {
+          method : "GET",
+          headers : {
+            "Authorization": `Bearer ${jwtToken}`,
+          }
+        })
+        const data = await response.json();
+        if (!data.foundLikeMdSuccess) {
+          console.error(data.message);
+        } else {
+          setMds(data.likedMd);
+          console.log(data.message)
+        }
       } catch (error) {
         console.log("LikeMdError", error)
       }
@@ -19,9 +35,9 @@ const LikeMd = () => {
 
     getMds()
 
-  }, [])
+  }, [userId, jwtToken])
 
-  // console.log(mds)
+  console.log(mds)
 
   return (
     <S.Container className='Container'>
@@ -29,7 +45,7 @@ const LikeMd = () => {
       { mds && mds.map((item, i) => (
         <S.Wrapper key={i} className='Wrapper'>
           <S.Image className='Image'>
-            <img src={item.mdImgUrl} alt='md 이미지' />
+            <img src={item.image} alt='md 이미지' />
           </S.Image>
           <S.Content className='Content'>
             <p>{item.mdName}</p>
