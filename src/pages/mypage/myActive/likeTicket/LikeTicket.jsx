960b7de0a1235861c6308ff1c5608694a22c7986
minkyong00 <minkyong131@gmail.com>
2025-01-05 22:21:17 +0900

    마이페이지 찜 티켓 목록 추가

--- src/pages/mypage/myActive/likeTicket/LikeTicket.jsx
+++ src/pages/mypage/myActive/likeTicket/LikeTicket.jsx
@@ -1,18 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import S from './LikeTicketStyle';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import usePagination from '../../../../hooks/usePagination';
+import LikeTicketComponent from './LikeTicketComponent';
+
+const PAGINATION = {
+  pageRange: 6,
+  btnRange: 3,
+};
 
 const LikeTicket = () => {
   const [ ticket, setTicket ] = useState([]);
+  const jwtToken = localStorage.getItem("jwtToken");
+
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  }
+
+  const { page, currentList, setPage, totalPost } = usePagination({
+    pageRange: PAGINATION.pageRange,
+    list: ticket || [],
+  });
+
 
   useEffect(() => {
     const getTicket = async () => {
      try {
-      const response = await fetch(`http://localhost:8000/my/like/ticket`);
-      const datas = await response.json();
-      setTicket(datas);
+      const response = await fetch(`http://localhost:8000/my/like/ticket`, {
+        method : "GET",
+          headers : {
+            "Authorization": `Bearer ${jwtToken}`
+          }
+      })
+        .then((res) => res.json())
+        .then((res) => {
+          if(!res.spaecSuccess){
+            console.log(res.message)
+          }
+          setTicket(res.likeTicket)
+          console.log(res.message)
+        })
      } catch (error) {
       console.log("LikeTicketError", error)
      }
@@ -22,36 +51,16 @@ const LikeTicket = () => {
 
   }, [])
 
-  const navigate = useNavigate();
-
-  const handleNavigate = (path) => {
-    navigate(path);
-  }
-
   return (
-    <div>
-      <S.Container className='Container'>
-        { ticket && ticket.map((item, i) => (
-          <S.Wrapper key={i} className='Wrapper' onClick={() => handleNavigate('/my-res/ticket/detail')}>
-            <S.RightContent className='rightContent'>
-              <p className='date'>{item.date}</p>
-              <p className='id'>예매번호 {item.id}</p>
-              <p className='ticketName'>{item.ticketName}</p>
-              <p className='qty'>{item.qty}매</p>
-              {/* <p className='cancellableDate'>취소 가능일 {item.cancellableDate}</p> */}
-              <p className='location'>{item.location}</p>
-              <p className='state'>{item.state}</p>
-            </S.RightContent>
-            <S.Img className='img'>
-              <img src={item.ticketImgUrl} alt="티켓 포스터 이미지" />
-              <div className='top'></div>
-              <div className='bottom'></div>
-            </S.Img>
-          </S.Wrapper>
-        ))
-        }
-      </S.Container>
-    </div>
+    <>
+      <LikeTicketComponent 
+        page={page} setPage={setPage} 
+        currentList={currentList} 
+        totalPost={totalPost}
+        PAGINATION={PAGINATION}
+        handleNavigate={handleNavigate}
+      />
+    </>
   );
 };
 
