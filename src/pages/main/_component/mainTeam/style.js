222f67b79c460bfc5ed35dd0454232d2bd73aff4
jiihyun12 <j22h.h0h@gmail.com>
2025-01-13 20:36:35 +0900

    메인 화면 디자인 / 커뮤니티   multer수정중

---
+++ src/pages/main/_component/mainTeam/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const S = {};
+
+
+
+S.Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color:#000; 
+  min-height: 100vh;
+  padding: 60px 20px;
+`;
+
+
+export default S;
